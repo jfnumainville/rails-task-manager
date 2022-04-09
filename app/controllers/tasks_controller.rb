ae5937bb20d3df0520a6ce58d819381d56d366d3
Jean-Francois Numainville <jfnumainville@gmail.com>
2022-04-09 15:16:40 -0400

    Finished the Rails Task Manager challenge

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,11 +1,11 @@
 class TasksController < ApplicationController
-
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
   def index
     @tasks = Task.all
   end
 
   def show
-    @task = Task.find(params[:id])
+
   end
 
   def new
@@ -15,9 +15,22 @@ class TasksController < ApplicationController
   def create
     @task = Task.new(task_params)
     @task.save
-    # redirect_to restaurant_path(@restaurant)
     redirect_to root_path
+  end
 
+  def edit
+
+  end
+
+  def update
+
+    @task.update(task_params)
+    redirect_to root_path
+  end
+
+  def destroy
+    @task .destroy
+    redirect_to root_path
   end
 
   private
@@ -26,5 +39,9 @@ class TasksController < ApplicationController
     params.require(:task).permit(:title, :details, :completed)
   end
 
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
 
 end
