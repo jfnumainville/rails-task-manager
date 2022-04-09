ad53c7e329a0e482e47c49c0c943fb99f63f1503
Jean-Francois Numainville <jfnumainville@gmail.com>
2022-04-09 14:14:30 -0400

    Finished Show, Add pages

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,30 @@
+class TasksController < ApplicationController
+
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+    # redirect_to restaurant_path(@restaurant)
+    redirect_to root_path
+
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
+
+
+end
