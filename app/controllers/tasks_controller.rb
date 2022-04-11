95bc1f57a72eb5ed88a22212900fef1517c784e5
Jean-Francois Numainville <jfnumainville@gmail.com>
2022-04-10 20:31:40 -0400

    Fixed some linked_to and redirect methods

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -15,7 +15,7 @@ class TasksController < ApplicationController
   def create
     @task = Task.new(task_params)
     @task.save
-    redirect_to root_path
+    redirect_to task_path(@task)
   end
 
   def edit
@@ -25,7 +25,7 @@ class TasksController < ApplicationController
   def update
 
     @task.update(task_params)
-    redirect_to root_path
+    redirect_to task_path(@task)
   end
 
   def destroy
